0b6f889dff376dfae3f19fbd5f48c1508fed99f4
HenrikasKr <kriauciunas.henrikas@gmail.com>
2023-04-20 21:44:03 +0300

    a

---
+++ js/custom.js
@@ -0,0 +1,73 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        }
+    }
+});
+
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+
+const onProgress = (event) => {
+    const progressBar = event.target.querySelector('.progress-bar');
+    const updatingBar = event.target.querySelector('.update-bar');
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+    if (event.detail.totalProgress === 1) {
+      progressBar.classList.add('hide');
+      event.target.removeEventListener('progress', onProgress);
+    } else {
+      progressBar.classList.remove('hide');
+    }
+  };
+  document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+  var bigPic = document.getElementById("big-pic");
+var Model3d = document.getElementById("model-3d");
+
+
+function changeImage(file){
+    Model3d.classList.add("hidden");
+        bigPic.classList.remove("hidden");
+    let img = document.querySelector("#big-pic");
+    img.setAttribute("src", file);
+}
+
+function model(){
+    if(Model3d.classList.contains("hidden")){
+        Model3d.classList.remove("hidden");
+        bigPic.classList.add("hidden");
+    }
+}
